9f9dc2ddbea66634c1ca76ee09a33c8273c9e4cb
SOLDATOV Maxime <maxime.soldatov@telecomnancy.eu>
2024-03-08 14:24:09 +0100

    Commit du fix de compilation

--- util.c
+++ util.c
@@ -2,7 +2,7 @@
 
 #include <string.h>
 
-void free_nothing(void *) {}
+void free_nothing(__attribute__((unused)) void *_) {}
 
 char not_strcmp(void *a, void *b) {
   return !(char)strcmp((char *)a, (char *)b);
