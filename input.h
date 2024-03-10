d69f2e13ed0db6ff5a44d16ccd219ccaccc7f474
Eliott Huet <eliott.huet@telecomnancy.eu>
2024-03-10 13:39:14 +0100

    changed comments to match the auto-generated doc standards

--- input.h
+++ input.h
@@ -6,62 +6,69 @@
 #include "ecs.h"
 
 typedef Uint8 KeyState;
-#define KEY_PRESSED 0  // the instant the key is pressed
-#define KEY_RELEASED 1 // the instant the key is released
-#define KEY_DOWN 2 // starts on press (included), ends on release (not included)
+//! the instant the key is pressed
+#define KEY_PRESSED 0
+//! the instant the key is released
+#define KEY_RELEASED 1
+//! starts on press (included), ends on release (not included)
+#define KEY_DOWN 2
 
-typedef Uint8 MouseButton; // describes any of the following: SDL_BUTTON_LEFT,
-                           // SDL_BUTTON_MIDDLE, SDL_BUTTON_RIGHT
+//! describes any of the following: SDL_BUTTON_LEFT,SDL_BUTTON_MIDDLE,
+//! SDL_BUTTON_RIGHT
+typedef Uint8 MouseButton;
 
-// stores keys and mouse buttons
+//! stores keys and mouse buttons
 typedef struct {
-  int *keys;     // uses SDL Scancodes as indices
-  Uint64 key_nb; // number of keys currently in
-  char mouse;    // 1st bit = mb_left; 2nd bit = mb_middle; 3rd bit = mb_right
+  //! uses SDL Scancodes as indices
+  int *keys;
+  //! number of keys currently in
+  Uint64 key_nb;
+  //! 1st bit = mb_left; 2nd bit = mb_middle; 3rd bit = mb_right
+  char mouse;
 } Inputs;
 
-// type of callback functions for the key events
+//! type of callback functions for the key events
 typedef void (*KeyEvent)(Entity *, Inputs *, KeyState);
 
-// creates a new Inputs instance
+//! creates a new Inputs instance
 Inputs *inputs_new();
 
-// frees the Inputs instance
+//! frees the Inputs instance
 void inputs_free(Inputs *);
 
-// the state of a key accessed using SDL_Scancode
-// !!!!!!!!!! this does not take into account non QWERTY keyboards / remaps
-// !!!!!!!!!! bool inputs_is_key_in_from_scancode(Input*,SDL_Scancode)
+//! the state of a key accessed using SDL_Scancode
+//! !!!!!!!!!! this does not take into account non QWERTY keyboards / remaps
+//! !!!!!!!!!! bool inputs_is_key_in_from_scancode(Input*,SDL_Scancode)
 #define inputs_is_key_in_from_scancode(inputs, scancode)                       \
   ((inputs)->keys[(scancode)])
 
-// the state of a key accessed using SDL_KeyCode
-// bool inputs_is_key_in(Inputs*, SDL_KeyCode)
+//! the state of a key accessed using SDL_KeyCode
+//! bool inputs_is_key_in(Inputs*, SDL_KeyCode)
 #define inputs_is_key_in(inputs, key)                                          \
   ((inputs)->keys[SDL_GetScancodeFromKey(key)])
 
-// the state of a mouse button
-// bool inputs_is_mouse_button_in(Inputs*,MouseButton)
+//! the state of a mouse button
+//! bool inputs_is_mouse_button_in(Inputs*,MouseButton)
 #define inputs_is_mouse_button_in(inputs, button)                              \
   (((inputs)->mouse >> ((button)-1)) & 1)
 
-// updates the state of a key using SDL_Scancode
-// !!!!!!!!!! this does not take into account non QWERTY keyboards / remaps
-// !!!!!!!!!! void inputs_update_key_in_from_scancode(Input*,SDL_Scancode,bool)
+//! updates the state of a key using SDL_Scancode
+//! !!!!!!!!!! this does not take into account non QWERTY keyboards / remaps
+//! !!!!!!!!!! void inputs_update_key_in_from_scancode(Input*,SDL_Scancode,bool)
 void inputs_update_key_in_from_scancode(Inputs *inputs, SDL_Scancode scancode,
                                         bool new_val);
 
-// updates the state of a key using SDL_KeyCode
-// void inputs_update_key_in(Input*,SDL_KeyCode,bool)
+//! updates the state of a key using SDL_KeyCode
+//! void inputs_update_key_in(Input*,SDL_KeyCode,bool)
 #define inputs_update_key_in(inputs, key, new_val)                             \
   (inputs_update_key_in_from_scancode(inputs, SDL_GetScancodeFromKey(key),     \
                                       new_val))
 
-// updates the state of a mouse button
-// MouseButton inputs_update_mouse_button_in(Input*,MouseButton,bool)
+//! updates the state of a mouse button
+//! MouseButton inputs_update_mouse_button_in(Input*,MouseButton,bool)
 #define inputs_update_mouse_button_in(inputs, button, new_val)                 \
   ((inputs)->mouse = (((!!(new_val)) << ((button)-1)) |                        \
                       ((inputs)->mouse & (~(1 << ((button)-1))))))
 
-// calls all the callbacks for the keyevent
+//! calls all the callbacks for the keyevent
 void inputs_run_callbacks(World *, Inputs *, KeyState);
