56f85e21001989abd9c3d7e15454e70c1471ac22
Ghislain <ghislain.mounier@telecomnancy.eu>
2024-03-26 15:22:27 +0100

    correction de bug sur les clickables et desactivation des hoverable

--- src/renderer/ui.c
+++ src/renderer/ui.c
@@ -29,6 +29,10 @@ void render_ui(World *w, SDL_Renderer *rdr) {
       SDL_SetTextureColorMod(c->sprite->texture, (Uint8)255, (Uint8)255,
                              (Uint8)255);
     }
+    if (!(c->is_clicked - 2)){
+      //script linked to clickable action 
+      c->is_clicked = 0;
+    }
   }
   mask = COMPF_MINIMAP;
   er = world_query(w, &mask);
@@ -39,12 +43,14 @@ void render_ui(World *w, SDL_Renderer *rdr) {
   }
   // To finish with SDL_TTF
   mask = COMPF_HOVERABLE;
-  er = world_query(w, &mask);
-  for (uint i = 0; i < vec_len(er); i++) {
-    Entity *e = get_entity(w, er[i]);
-    Hoverable *h = entity_get_component(w, e, COMP_HOVERABLE);
-    //   if (mouse_in_rect(h->rect))
-    //     render_hoverable(h->rect, h->text);
+  // printf("1\n ");
+  // er = world_query(w, &mask);
+  // printf("1\n ");
+  // for (uint i = 0; i < vec_len(er); i++) {
+  //   Entity *e = get_entity(w, er[i]);
+  //   Hoverable *h = entity_get_component(w, e, COMP_HOVERABLE);
+  //   //   if (mouse_in_rect(h->rect))
+  //   //     render_hoverable(h->rect, h->text);
   }
 }
 
@@ -56,10 +62,10 @@ Entity *spawn_clickable(World *w, Clickable *object, KeyEvent *event) {
 }
 
 void clickable_event(World *w, Entity *entity, Inputs *in, KeyState keystate) {
-  if (!inputs_is_mouse_button_in(in, SDL_BUTTON_LEFT))
-    return;
+  // if (!inputs_is_mouse_button_in(in, SDL_BUTTON_LEFT))
+  //   return;
   Clickable *c = entity_get_component(w, entity, COMP_CLICKABLE);
-
+  
   if (mouse_in_rect(c->sprite->rect)) {
     if (keystate == KEY_PRESSED) {
       c->is_clicked = 1;
