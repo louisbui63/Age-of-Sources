9e17786523c5c924d79319685df6a4420bda72f4
Ghislain <ghislain.mounier@telecomnancy.eu>
2024-04-17 13:00:17 +0200

    fin de parser.*

---
+++ src/units/unit_template.h
@@ -0,0 +1,58 @@
+//! @file unit_template.h
+#pragma once
+
+//! This file is a template for the unit files, every data about the units must
+//! be put between `/*`and `*/` and nothing else will use this kind of
+//! commentary.
+
+//! The first text is the name of the unit. It must not be more than 255
+//! characters long.
+/*Name*/
+
+//! The second text is the HP of the unit, it must a be positive decimal number
+//! smaller or equal than 65535
+/*000*/
+
+//! The third text is the BDam of the unit, it must a be positive decimal
+//! number smaller or equal than 65535
+/*000*/
+
+//! The fourth text is the PDam of the unit, it must a be positive decimal
+//! number smaller or equal than 65535
+/*000*/
+
+//! The fifth text is the SDam of the unit, it must a be positive decimal
+//! number smaller or equal than 65535
+/*000*/
+
+//! The sixth text is the BDef of the unit, it must a be positive decimal
+//! number smaller or equal than 65535
+/*000*/
+
+//! The seventh text is the PDef of the unit, it must a be positive decimal
+//! number smaller or equal than 65535
+/*000*/
+
+//! The eigth text is the SDef of the unit, it must a be positive decimal
+//! number smaller or equal than 65535
+/*000*/
+
+//! The ninth text is the Rg of the unit, it must a be positive decimal
+//! number smaller or equal than 65535
+/*000*/
+
+//! The tenth text is the Sp of the unit, it must a be positive decimal
+//! number smaller or equal than 65535
+/*000*/
+
+//! The elventh text is the w of the unit's sprite, it must a be positive
+//! decimal number smaller or equal than 65535
+/*000*/
+
+//! The twelfth text is the h of the unit's sprite, it must a be positive
+//! decimal number smaller or equal than 65535
+/*000*/
+
+//! The thirteenth text is the unit's sprite path, it must be less than 255
+//! characters long.
+/*path/to/sprite.bmp*/
