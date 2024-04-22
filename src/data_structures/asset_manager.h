ad356e64d25c4e55e959ca46132ef258d3752efc
MOUNIER Ghislain <ghislain.mounier@telecomnancy.eu>
2024-04-22 07:21:02 +0000

    Le retour du texte disparu

--- src/data_structures/asset_manager.h
+++ src/data_structures/asset_manager.h
@@ -26,6 +26,19 @@ void *get_texture(char *t, SDL_Renderer *renderer, SDL_Window *window);
 //! unadvisable as slow. Crashes on invalid file path or texture creation.
 void *load_texture(char *t, SDL_Renderer *renderer, SDL_Window *window);
 
+//! Returns a pointer to the audio from file `t`. Will had it to the
+//! `ASSET_STORE` if it is not in it yet
+int drop_texture(char *t);
+
+void *get_audio(char *t, char is_mus);
+
+//! Loads the audio from file `t` in the `ASSET_STORE`
+//! While calling it multiple times with the same `t` shouldn't fail, it is
+//! unadvisable as slow. Crashes on invalid file path or audio creation.
+void *load_audio(char *t, char is_mus);
+
+int drop_audio(char *t);
+
 void *load_font(char *t, Uint8 size);
 
 //! Returns a pointer to the font from file `t`. Will add it to the
