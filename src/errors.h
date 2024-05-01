d6eced24cc43da2e21d0e3368954896bc3ff6de5
Louis Buisson <Louis.Buisson@telecomnancy.eu>
2024-05-01 17:47:49 +0200

    doc

--- src/errors.h
+++ src/errors.h
@@ -1,10 +1,20 @@
+//! @file input.h
 #pragma once
 
+//! a general error return type
 typedef enum {
+  //! self explanatory
   SUCCESS = 0,
+  //! function call couldn't succeed because of insufficiant memory in the heap
   OUT_OF_MEMORY,
+  //! function was called with invalid parameters leading to access out of the
+  //! allowed range
   INDEX_OUT_OF_RANGE,
+  //!
   INVALID_TEXTURE,
+  //! asset doesn't exist, i.e. it is not loaded in the asset manager and there
+  //! is no file corresponding to the requested asset
   ASSET_NOT_FOUND,
+  //! self explanatory
   ASSERTION_FAILED
 } Error;
