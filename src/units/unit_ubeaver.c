12ecd28389e01c62d22384ee15c8e0d72cb238cb
eliott <eliott.huet@telecomnancy.eu>
2024-05-25 18:10:48 +0200

    smol fix

--- src/units/unit_ubeaver.c
+++ src/units/unit_ubeaver.c
@@ -61,10 +61,8 @@
 //! characters long.
 /*A mercenary worker that works for whoever pays them*/
 
-void ubeaver_grid(World *w, int slot) {
-  if (slot == 0) {
-    char *c = malloc(sizeof(char) * (strlen("src/units/unit_well.c") + 1));
-    strcpy(c, "src/units/unit_well.c");
-    set_building_selection(w, c, WELL);
-  }
+void ubeaver_grid(__attribute__((unused)) World *w,
+                        __attribute__((unused)) int slot,
+                        __attribute__((unused)) Entity *e) {
+  return empty_click_event;
 }
